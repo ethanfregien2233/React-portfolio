33b10bc9c865e3cd0b9c0bc603e2d4d5dbc66384
ethan fregien <ethanfregien@gmail.com>
2022-01-27 15:35:25 -0600

    more setup and file creations

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+import About from './components/About';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Switch>
+          <Route 
+            exact path="/projects" component={Projects} 
+          />
+          <Route 
+            exact path="/about" component={About}
+          />
+          <Route 
+            exact path="/contact" component={Contact}
+          />
+          <Route 
+            exact path="/resume" component={Resume}
+          />
+          <Route
+            component={About}
+          />
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
